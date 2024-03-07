277ba81a801e79bf038a9ab16e76f58e80f5b541
tikkhonov <a.tikkhonov@outlook.com>
2024-03-07 21:11:35 +0300

    add css-extractor

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,9 @@
 import { render } from "react-dom";
-import React from "react";
+import App from "./App";
 
-render(<div>App</div>, document.getElementById("root"));
+render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
