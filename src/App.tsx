262ce82c226c1cfe4f1cf11819cd574542e0462e
tikkhonov <a.tikkhonov@outlook.com>
2024-03-09 19:23:23 +0300

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./index.scss";
+import { Link } from "react-router-dom";
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { Suspense } from "react";
 
 const App = () => {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <Link to="/">Main</Link>
+      <Link to="/about">About</Link>
+      <Suspense fallback={<div> "Loading..."</div>}>
+        <Routes>
+          <Route path={"/"} element={<MainPageLazy />} />
+          <Route path={"/about"} element={<AboutPageLazy />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
