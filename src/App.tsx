b7eac564fd3b4b4d3b070caf7eed2b4b082ef10f
tikkhonov <a.tikkhonov@outlook.com>
2024-03-12 16:47:57 +0300

    add classNames

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
 import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
 import { Suspense } from "react";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 export enum Theme {
   LIGHT = "light",
@@ -15,7 +16,7 @@ const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
