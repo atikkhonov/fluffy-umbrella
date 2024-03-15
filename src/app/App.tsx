7d9e4280b2c3c99ae82d6fdd944f60849163a0ef
tikkhonov <a.tikkhonov@outlook.com>
2024-03-15 22:23:38 +0300

    add navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "app/providers/router";
+import { Navbar } from "widgets/Navbar";
 
 export enum Theme {
   LIGHT = "light",
@@ -15,8 +16,7 @@ const App = () => {
   return (
     <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to="/">Main</Link>
-      <Link to="/about">About</Link>
+      <Navbar />
       <AppRouter />
     </div>
   );
