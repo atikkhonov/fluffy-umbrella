05c2c9ffdd5c7dcdc1236f39066b741f49dcfcc2
tikkhonov <a.tikkhonov@outlook.com>
2024-03-25 20:09:51 +0300

    add bundle-analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,31 +1,34 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+
 import {
-    DefinePlugin,
-    HotModuleReplacementPlugin,
-    ProgressPlugin,
-    WebpackPluginInstance,
-} from 'webpack';
-import { BuildOptions } from './types/config';
+  DefinePlugin,
+  HotModuleReplacementPlugin,
+  ProgressPlugin,
+  WebpackPluginInstance,
+} from "webpack";
+import { BuildOptions } from "./types/config";
 
 export function buildPlugins({
-    paths,
-    isDev,
+  paths,
+  isDev,
 }: BuildOptions): WebpackPluginInstance[] {
-    return [
-        new HtmlWebpackPlugin({
-            template: paths.html,
-        }),
-        new ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }),
-        new DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev),
-        }),
+  return [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css",
+    }),
+    new DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+    new HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin(),
     // new ReactRefreshWebpackPlugin(),
-    // new HotModuleReplacementPlugin(),
-    ];
+  ];
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "app/providers/ThemeProvider";
 import { ErrorBoundary } from "app/providers/ErrorBoundary";
